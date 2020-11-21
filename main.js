f8b71e5c54c656491500a26e28188ba232a27583
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-11-21 20:18:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ var ball = {
 }
 
 function setup(){
-  var canvas=createCanvas(700,470);
+  var canvas=createCanvas(700,600);
   canvas.parent('canvas');
   video=createCapture(VIDEO);
   video.hide();
