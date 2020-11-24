eab58d6a4da885dda5d3167f74036535d38af06d
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-11-24 19:49:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -49,6 +49,12 @@ function gotPoses(results){
   }
 }
 
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
+}
+
 function draw(){
  background(0); 
  
@@ -74,42 +80,44 @@ function draw(){
    circle(rightWristX, rightWristY, 25);
  }
  
-   //funtion paddleInCanvas call 
-   paddleInCanvas();
- 
-   //left paddle
-   fill(250,0,0);
-    stroke(0,0,250);
-    strokeWeight(0.5);
-   paddle1Y = mouseY; 
-   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
-   
-   
-    //pc computer paddle
-    fill("#FFA500");
-    stroke("#FFA500");
-   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
-    
-    //function midline call
-    midline();
-    
-    //funtion drawScore call 
-   drawScore();
-   
-   //function models call  
-   models();
-   
-   //function move call which in very important
-    move();
-}
+ if(game_status == "start"){
+     //funtion paddleInCanvas call 
+     paddleInCanvas();
 
-//function reset when ball does notcame in the contact of padde
-function reset(){
-   ball.x = width/2+100,
-   ball.y = height/2+100;
-   ball.dx=3;
-   ball.dy =3;
-   
+     //left paddle
+     fill(250,0,0);
+      stroke(0,0,250);
+      strokeWeight(0.5);
+     paddle1Y = mouseY; 
+     rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+     
+     
+      //pc computer paddle
+      fill("#FFA500");
+      stroke("#FFA500");
+     var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+      
+      //function midline call
+      midline();
+      
+      //funtion drawScore call 
+      drawScore();
+     
+     //function models call  
+      models();
+     
+     //function move call which in very important
+      move();
+  }
+  
+  //function reset when ball does notcame in the contact of padde
+  function reset(){
+     ball.x = width/2+100,
+     ball.y = height/2+100;
+     ball.dx=3;
+     ball.dy =3;
+ }
+
 }
 
 
